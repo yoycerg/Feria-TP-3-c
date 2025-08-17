dce22fcabfccb70f2d6f38d87d9b76f497487c8a
yoycerg <yoicergarcia@liceovvh.cl>
2025-08-17 13:27:27 -0400

    avance

--- static/js/inicio.js
+++ static/js/inicio.js
@@ -17,3 +17,25 @@ function irInicioSesion() {
   window.location.href = "pages/inicio_sesion.html";
 
 }
+
+
+function mostrarUsuario() {
+  let nombre = localStorage.getItem("usuario");
+  let foto = localStorage.getItem("foto");
+
+  if (nombre && foto) {
+    document.getElementById("user-area").innerHTML = `
+          <img src="${foto}" class="profile-pic" alt="Foto de perfil">
+          <span>${nombre}</span>
+       
+        `;
+  }
+}
+
+function cerrarSesion() {
+  localStorage.removeItem("usuario");
+  localStorage.removeItem("foto");
+  location.reload();
+}
+
+window.onload = mostrarUsuario;
--- static/js/inicio_sesion.js
+++ static/js/inicio_sesion.js
@@ -1,2 +1,12 @@
+function guardarUsuario(e) {
+    e.preventDefault();
+    let nombre = document.getElementById("nombre").value;
 
+    // Guardamos el nombre en localStorage
+    localStorage.setItem("usuario", nombre);
+    localStorage.setItem("foto", "https://i.pravatar.cc/100"); // foto demo
+
+    // Redirigir al index.html
+    window.location.href = "../../index.html";
+}
 
