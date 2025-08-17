110be1b84f1e222a2b848f006da6396105cae53d
yoycerg <yoicergarcia@liceovvh.cl>
2025-08-17 13:40:15 -0400

    avance

--- static/js/inicio_sesion.js
+++ static/js/inicio_sesion.js
@@ -6,7 +6,15 @@
         document.getElementById("user-area").innerHTML = `
           <img src="${foto}" class="profile-pic" alt="Foto de perfil">
           <span>${nombre}</span>
-       
+          <button onclick="cerrarSesion()">Cerrar Sesión</button>
         `;
       }
     }
+
+    function cerrarSesion() {
+      localStorage.removeItem("usuario");
+      localStorage.removeItem("foto");
+      location.reload();
+    }
+
+    window.onload = mostrarUsuario;
